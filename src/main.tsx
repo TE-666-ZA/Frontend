c14351cfd496af1cf2938a08e41bfd20c014a8d1
Kenan Iusubovi <te666za@gmail.com>
2023-11-12 03:58:58 +0100

    last fixes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import GetData from "./resFromDrive/GetData";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
